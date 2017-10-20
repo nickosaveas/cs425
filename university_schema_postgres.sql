98d2d476f37915da93c94df7072c79e2c22a2f2c
nickosaveas <nsaveas@hawk.iit.edu>
2017-10-20 12:09:53 -0500

    Update university_schema_postgres.sql
    Troubleshooting is stupid

--- university_schema_postgres.sql
+++ university_schema_postgres.sql
@@ -5,8 +5,6 @@ DROP TABLE IF EXISTS faculty CASCADE;
 DROP TABLE IF EXISTS enroll CASCADE;
 DROP TABLE IF EXISTS book_checkout CASCADE;
 
-----------------------------------------------------------------------
-
 CREATE TABLE book
 		(
 			bookid       int,
@@ -15,15 +13,20 @@ CREATE TABLE book
 			total_copies int,
 			PRIMARY KEY  (bookid)
 		);
+		
+CREATE TABLE course
+		(
+			courseid 	  int,
+			title         varchar(50),
+			instructorid  int,
+			textbookid    int,
+			PRIMARY KEY   (courseid),
+			FOREIGN KEY   (instructorid) REFERENCES faculty
+						on DELETE CASCADE,
+			FOREIGN KEY   (textbookid) REFERENCES book
+						on DELETE CASCADE
+		);
 
-DELETE FROM book;
-
-INSERT INTO book VALUES ('1', 'Introduction of Algorithms', '84.66', '4');
-INSERT INTO book VALUES ('2', 'Database System Concepts', '74.99', '5');
-INSERT INTO book VALUES ('3', 'Stochastic Calculus for Finance I', '41.02', '3');
-INSERT INTO book VALUES ('4', 'Stochastic Calculus for Finance II', '55.22', '3');
-
--------------------------------------------------------------------------------------
 CREATE TABLE student
 		(
 			studentid    int,
@@ -31,16 +34,6 @@ CREATE TABLE student
 			gpa          numeric(3,0),
 			PRIMARY KEY  (studentid)
 		);
-		
-DELETE FROM student;
-
-INSERT INTO student VALUES ('1', 'Tom', '3.3');
-INSERT INTO student VALUES ('2', 'John', '3.8');
-INSERT INTO student VALUES ('3', 'Mary', '3.0');
-INSERT INTO student VALUES ('4', 'Kris', '3.6');
-INSERT INTO student VALUES ('5', 'Alex', '3.5');
-
----------------------------------------------------------------------------------
 		
 CREATE TABLE faculty
 		(
@@ -50,54 +43,16 @@ CREATE TABLE faculty
 			PRIMARY KEY  (facultyid)
 		);
 		
-DELETE FROM faculty;
-
-INSERT INTO faculty VALUES ('1', 'James', '70000');
-INSERT INTO faculty VALUES ('2', 'Sarah', '60000');
-INSERT INTO faculty VALUES ('3', 'Jay', '80000');
-INSERT INTO faculty VALUES ('4', 'Rachel', '70000');
-INSERT INTO faculty VALUES ('5', 'Paul', '85000');
-		
-CREATE TABLE course
-		(
-			courseid 	  int,
-			title         varchar(50),
-			instructorid  int,
-			textbookid    int,
-			PRIMARY KEY   (courseid),
-			FOREIGN KEY   (instructorid) REFERENCES faculty on DELETE CASCADE,
-			FOREIGN KEY   (textbookid) REFERENCES book on DELETE CASCADE
-		);
-		
-DELETE FROM course;
-
-INSERT INTO course VALUES ('1', 'Algorithms', '1' ,'1');
-INSERT INTO course VALUES ('2', 'DB Organization', '2' ,'2');
-INSERT INTO course VALUES ('3', 'Advanced DB Organization', '3' ,'2');
-INSERT INTO course VALUES ('4', 'Math Finance I', '1' ,'3');
-INSERT INTO course VALUES ('5', 'Math Finance II', '4' ,'4');
-
---------------------------------------------------------------------------
-		
 CREATE TABLE enroll
 		(
 			studentid    int,
 			courseid     int,
 			PRIMARY KEY  (studentid, courseid),
-			FOREIGN KEY  (studentid) REFERENCES student on DELETE CASCADE
-			FOREIGN KEY  (courseid) REFERENCES course on DELETE CASCADE
+			FOREIGN KEY  (studentid) REFERENCES student
+						on DELETE CASCADE,
+			FOREIGN KEY  (courseid) REFERENCES course
+						on DELETE CASCADE
 		);
-		
-DELETE FROM enroll;
-
-INSERT INTO enroll VALUES ('1', '1');
-INSERT INTO enroll VALUES ('1', '2');
-INSERT INTO enroll VALUES ('2', '1');
-INSERT INTO enroll VALUES ('4', '3');
-INSERT INTO enroll VALUES ('4', '4');
-INSERT INTO enroll VALUES ('5', '5');
-
-------------------------------------------------------------------------------------------
 
 CREATE TABLE book_checkout
 		(
@@ -105,14 +60,48 @@ CREATE TABLE book_checkout
 			bookid         int,
 			studentid      int,
 			PRIMARY KEY    (bookid, studentid),
-			FOREIGN KEY    (bookid) REFERENCES book on DELETE CASCADE
-			FOREIGN KEY    (studentid) REFERENCES student on DELETE CASCADE
+			FOREIGN KEY    (bookid) REFERENCES book
+						on DELETE CASCADE,
+			FOREIGN KEY    (studentid) REFERENCES student
+						on DELETE CASCADE
 		);
 		
-
-
+DELETE FROM book;
+DELETE FROM course;
+DELETE FROM student;
+DELETE FROM faculty;
+DELETE FROM enroll;
 DELETE FROM book_checkout;
 
+INSERT INTO book VALUES ('1', 'Introduction of Algorithms', '84.66', '4');
+INSERT INTO book VALUES ('2', 'Database System Concepts', '74.99', '5');
+INSERT INTO book VALUES ('3', 'Stochastic Calculus for Finance I', '41.02', '3');
+INSERT INTO book VALUES ('4', 'Stochastic Calculus for Finance II', '55.22', '3');
+
+INSERT INTO course VALUES ('1', 'Algorithms', '1' ,'1');
+INSERT INTO course VALUES ('2', 'DB Organization', '2' ,'2');
+INSERT INTO course VALUES ('3', 'Advanced DB Organization', '3' ,'2');
+INSERT INTO course VALUES ('4', 'Math Finance I', '1' ,'3');
+INSERT INTO course VALUES ('5', 'Math Finance II', '4' ,'4');
+
+INSERT INTO student VALUES ('1', 'Tom', '3.3');
+INSERT INTO student VALUES ('2', 'John', '3.8');
+INSERT INTO student VALUES ('3', 'Mary', '3.0');
+INSERT INTO student VALUES ('4', 'Kris', '3.6');
+INSERT INTO student VALUES ('5', 'Alex', '3.5');
+
+INSERT INTO faculty VALUES ('1', 'James', '70000');
+INSERT INTO faculty VALUES ('2', 'Sarah', '60000');
+INSERT INTO faculty VALUES ('3', 'Jay', '80000');
+INSERT INTO faculty VALUES ('4', 'Rachel', '70000');
+INSERT INTO faculty VALUES ('5', 'Paul', '85000');
+
+INSERT INTO enroll VALUES ('1', '1');
+INSERT INTO enroll VALUES ('1', '2');
+INSERT INTO enroll VALUES ('2', '1');
+INSERT INTO enroll VALUES ('4', '3');
+INSERT INTO enroll VALUES ('4', '4');
+INSERT INTO enroll VALUES ('5', '5');
 
 INSERT INTO book_checkout VALUES ('2017-08-29', '1', '1');
 INSERT INTO book_checkout VALUES ('2017-09-02', '4', '4');
