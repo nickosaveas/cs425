62e400ad3e8bef189e5b56b69a5fca01486080de
nickosaveas <nsaveas@hawk.iit.edu>
2017-11-21 11:32:25 -0600

    Re purpose sql statements to fit Final project

--- university_schema_postgres.sql
+++ university_schema_postgres.sql
@@ -1,108 +1,173 @@
-DROP TABLE IF EXISTS book CASCADE;
-DROP TABLE IF EXISTS course CASCADE;
-DROP TABLE IF EXISTS student CASCADE;
-DROP TABLE IF EXISTS faculty CASCADE;
-DROP TABLE IF EXISTS enroll CASCADE;
-DROP TABLE IF EXISTS book_checkout CASCADE;
+CREATE TABLE Address
+{
+	street_name_num	varchar(100),
+	zip				varchar(9),
+	city			varchar(50),
+	state			varchar(25),
+	country			varchar(50),
+	addressType		varchar(20),
+	PRIMARY KEY (streetAddr, zip)
+}
 
-CREATE TABLE book
-		(
-			bookid       int,
-			title 		 varchar(50),
-			price 		 numeric(5, 0),
-			total_copies int,
-			PRIMARY KEY  (bookid)
-		);
+CREATE TABLE Airport
+{
+	IATA_airportCode varchar(5),
+	airportName		 varchar(100),
+	country			 varchar(50),
+	city			 varchar(50),
+	state			 varchar(25),
+	PRIMARY KEY 	 IATA_airportCode
+}
 
-CREATE TABLE student
-		(
-			studentid    int,
-			name 		 varchar(25),
-			gpa          numeric(3,0),
-			PRIMARY KEY  (studentid)
-		);
-		
-CREATE TABLE faculty
-		(
-			facultyid    int,
-			name 		 varchar(25),
-			salary       numeric(8,0),
-			PRIMARY KEY  (facultyid)
-		);
-		
-CREATE TABLE course
-		(
-			courseid 	  int,
-			title         varchar(50),
-			instructorid  int,
-			textbookid    int,
-			PRIMARY KEY   (courseid),
-			FOREIGN KEY   (instructorid) REFERENCES faculty
-						on DELETE CASCADE,
-			FOREIGN KEY   (textbookid) REFERENCES book
-						on DELETE CASCADE
-		);
-		
-CREATE TABLE enroll
-		(
-			studentid    int,
-			courseid     int,
-			PRIMARY KEY  (studentid, courseid),
-			FOREIGN KEY  (studentid) REFERENCES student
-						on DELETE CASCADE,
-			FOREIGN KEY  (courseid) REFERENCES course
-						on DELETE CASCADE
-		);
+CREATE TABLE Airline
+{
+	AirlineID		varchar(5),
+	name 			varchar(100),
+	country_origin 	varchar(50),
+	PRIMARY KEY AirlineID
+}
 
-CREATE TABLE book_checkout
-		(
-			checkout_date  date,
-			bookid         int,
-			studentid      int,
-			PRIMARY KEY    (bookid, studentid),
-			FOREIGN KEY    (bookid) REFERENCES book
-						on DELETE CASCADE,
-			FOREIGN KEY    (studentid) REFERENCES student
-						on DELETE CASCADE
-		);
-		
-DELETE FROM book;
-DELETE FROM course;
-DELETE FROM student;
-DELETE FROM faculty;
-DELETE FROM enroll;
-DELETE FROM book_checkout;
+CREATE TABLE Seat_Class
+{
+	seatClass_ID	varchar(5),
+	name			varchar(50),
+	PRIMARY KEY (seatClass_ID, name),
+	CHECK (seatClass_ID = 'F') OR (seatClass_ID = 'E'),
+	CHECK (name = 'First Class') OR (name = 'Economy')
+}
 
-INSERT INTO book VALUES ('1', 'Introduction of Algorithms', '84.66', '4');
-INSERT INTO book VALUES ('2', 'Database System Concepts', '74.99', '5');
-INSERT INTO book VALUES ('3', 'Stochastic Calculus for Finance I', '41.02', '3');
-INSERT INTO book VALUES ('4', 'Stochastic Calculus for Finance II', '55.22', '3');
+CREATE TABLE Flight_Seat
+{
+	AirlineID		varchar(5),
+	seat_ID			varchar(20),
+	seatClass_ID	varchar(5),
+	name			varchar(50),
+	seatNum 		varchar(5),
+	PRIMARY KEY (AirlineID, seat_ID, seatClass_ID),
+	FOREIGN KEY AirlineID REFERENCES Airline
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (seatClass_ID, name) REFERENCES Seat_Class
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+}
 
-INSERT INTO student VALUES ('1', 'Tom', '3.3');
-INSERT INTO student VALUES ('2', 'John', '3.8');
-INSERT INTO student VALUES ('3', 'Mary', '3.0');
-INSERT INTO student VALUES ('4', 'Kris', '3.6');
-INSERT INTO student VALUES ('5', 'Alex', '3.5');
+CREATE TABLE Flight_Path
+{
+	departure_iata 	 varchar(5),
+	destination_iata varchar(5),
+	PRIMARY KEY (departure_iata, destination_iata),
+	FOREIGN KEY departure_iata REFERENCES Airport
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY destination_iata REFERENCES Airport
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+}
 
-INSERT INTO faculty VALUES ('1', 'James', '70000');
-INSERT INTO faculty VALUES ('2', 'Sarah', '60000');
-INSERT INTO faculty VALUES ('3', 'Jay', '80000');
-INSERT INTO faculty VALUES ('4', 'Rachel', '70000');
-INSERT INTO faculty VALUES ('5', 'Paul', '85000');
+CREATE TABLE Flight_Schedule
+{
+	schedule_ID		 varchar(50),
+	departure_iata   varchar(5),
+	destination_iata varchar(5),
+	depart_time 	 time,
+	arrival_time 	 time,
+	PRIMARY KEY schedule_ID,
+	FOREIGN KEY (departure_iata, destination_iata) REFERENCES Flight_Path
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 
-INSERT INTO course VALUES ('1', 'Algorithms', '1' ,'1');
-INSERT INTO course VALUES ('2', 'DB Organization', '2' ,'2');
-INSERT INTO course VALUES ('3', 'Advanced DB Organization', '3' ,'2');
-INSERT INTO course VALUES ('4', 'Math Finance I', '1' ,'3');
-INSERT INTO course VALUES ('5', 'Math Finance II', '4' ,'4');
+}
 
-INSERT INTO enroll VALUES ('1', '1');
-INSERT INTO enroll VALUES ('1', '2');
-INSERT INTO enroll VALUES ('2', '1');
-INSERT INTO enroll VALUES ('4', '3');
-INSERT INTO enroll VALUES ('4', '4');
-INSERT INTO enroll VALUES ('5', '5');
+CREATE TABLE Flight
+{
+	AirlineID			varchar(5),
+	flightNum   		varchar(20),
+	date				date,
+	schedule_ID 		varchar(50),
+	maxSeats_firstclass int,
+	maxSeat_economy     int,
+	PRIMARY KEY (AirlineID, flightNum, date, schedule_ID)
+	FOREIGN KEY AirlineID REFERENCES Airline
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY schedule_ID REFERENCES Flight_Schedule
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
+}
 
-INSERT INTO book_checkout VALUES ('2017-08-29', '1', '1');
-INSERT INTO book_checkout VALUES ('2017-09-02', '4', '4');
-INSERT INTO book_checkout VALUES ('2017-09-07', '1', '4');
+CREATE TABLE Customer
+{
+	email_address	varchar(100),
+	name_first		varchar(25),
+	name_last		varchar(25),
+	name_midInt		char(1),
+	address1		varchar(100),
+	zip1			varchar(9),
+	address2		varchar(100),
+	zip2			varchar(9),
+	iata_hm_airport	varchar(3),
+	PRIMARY KEY email_address,
+	FOREIGN KEY iata_hm_airport REFERENCES Airport
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (address1, zip1) REFERENCES Address
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (address2, zip2) REFERENCES Address
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
+}
+
+CREATE TABLE Booking
+{
+	email_address	varchar(100),
+	AirlineID		varchar(5),
+	flightNum		varchar(20),
+	date 			date,
+	seatID			varchar(20),
+	PRIMARY KEY (email_address, AirlineID, flightNum, date, seatID),
+	FOREIGN KEY email_address REFERENCES Customer
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY AirlineID REFERENCES Airline
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+}
+
+CREATE TABLE Booked_Seat
+{
+	email_address	varchar(100),
+	AirlineID		varchar(5),
+	flightNum		varchar(20),
+	date 			date,
+	seatID			varchar(20),
+	name_first		varchar(25),
+	name_last		varchar(25),
+	name_midInt		varchar(1),
+	price			numeric(10, 2),
+	PRIMARY KEY (email_address, AirlineID, flightNum, date, seatID, price),
+	FOREIGN KEY email_address REFERENCES Customer
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY AirlineID REFERENCES Airline
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+}
+
+CREATE TABLE Credit_Card
+{
+	card_number		char(16),
+	exp_date		varchar(5),
+	security_code 	varchar(3),
+	email_address	varchar(100),
+	payment_address varchar(100),
+	payment_zip		varchar(9),
+	PRIMARY KEY card_number,
+	FOREIGN KEY email_address REFERENCES Customer
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (payment_address, payment_zip) REFERENCES Address
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
+}
